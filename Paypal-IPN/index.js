bcdb9d073d51443d3ec30aaa450e37f71c9ba482
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2020-03-30 14:57:00 -0700

    init

---
+++ Paypal-IPN/index.js
@@ -0,0 +1,105 @@
+'use strict'
+
+// Imports
+var querystring = require('querystring')
+
+var Mailchimp = require('mailchimp-api-v3')
+var bent = require('bent') // A good HTTP client
+
+// Keys and other application settings
+var API_KEY = process.env['MAILCHIMP_API_KEY']
+var LIST_ID = process.env['MAILCHIMP_LIST_ID']
+
+/**
+ * @const {boolean} sandbox Indicates if the sandbox endpoint is used.
+ */
+const sandbox = Boolean(process.env['PAYPAL_SANDBOX']);
+
+/** Production Postback URL */
+const PRODUCTION_VERIFY_URI = 'https://ipnpb.paypal.com/cgi-bin/webscr';
+/** Sandbox Postback URL */
+const SANDBOX_VERIFY_URI = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr';
+
+/**
+ * Determine endpoint to post verification data to.
+ *
+ * @return {String}
+ */
+function getPaypalURI() {
+  return sandbox ? SANDBOX_VERIFY_URI : PRODUCTION_VERIFY_URI;
+}
+
+// API setup
+var mailchimp = new Mailchimp(API_KEY)
+var paypal_verify = bent(getPaypalURI(), 'POST', 'string')
+
+module.exports = async function (context, req) {
+    if (req.method !== 'POST') {
+        context.log(`Request method not allowed. Was: ${req.method}`)
+        context.res.statusCode = 405
+        context.res.send('Method Not Allowed')
+        return
+    }
+    context.log('PayPal IPN Notification Event received successfully.')
+
+    if (sandbox) {
+        context.log('SANDBOX: Using paypal sandbox environment')
+    }
+
+    // JSON object of the IPN message consisting of transaction details.
+    let ipnTransactionMessage = querystring.parse(req.body);
+    // req.body is not parsed by Azure and is urlencoded.
+    // Build the body of the verification post message by prefixing 'cmd=_notify-validate'.
+    let verificationBody = `cmd=_notify-validate&${req.body}`;
+
+    const verifyResponse = await paypal_verify('/', verificationBody)
+    if (verifyResponse === 'VERIFIED') {
+        context.log(
+            `Verified IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is verified.`
+        )
+    } else if (verifyResponse === 'INVALID') {
+        context.log(
+            `Invalid IPN: IPN message for Transaction ID: ${ipnTransactionMessage.txn_id} is invalid.`
+        )
+        return
+    } else {
+        context.log(`Invalid IPN: Unexpected IPN verify response body: ${verifyResponse}`);
+        return
+    }
+
+    if (ipnTransactionMessage.payment_status !== 'Completed') {
+        context.log(`Payment status was not "Completed": ${ipnTransactionMessage.payment_status}`)
+        return
+    }
+
+    context.log(`Mailchimp: ${ipnTransactionMessage.payer_email}`)
+
+    var dateNow = new Date(Date.now())
+    var dateExpires = new Date(Date.now())
+    dateExpires.setFullYear(dateNow.getFullYear() + 5)
+    try {
+        var result = await mailchimp.post(`/lists/${LIST_ID}/members`, {
+            email_address: ipnTransactionMessage.payer_email,
+            merge_fields: {
+                FNAME: ipnTransactionMessage.first_name,
+                LNAME: ipnTransactionMessage.last_name,
+                JOINED: dateNow.toISOString(),
+                EXPIRES: dateExpires.toISOString()
+            },
+            status: 'pending'
+        })
+    } catch (err) {
+        context.log('Mailchimp: errors:', err.errors)
+        throw err
+    }
+    if (result.statusCode === 200 &&
+        (result.status === 'pending' || result.status === 'subscribed')) {
+        context.log(`Mailchimp: Successfully subscribed: ${result.email_address}`)
+
+        context.res.statusCode = 200
+    } else {
+        context.log('Mailchimp: Unsuccessful result:', result)
+        context.res.statusCode = 500
+    }
+    context.res.end()
+};
